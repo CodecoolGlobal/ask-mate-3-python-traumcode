ea773d9bd0c4fb460da39730c86c52c10d7a0d30
Simina-Elena <siminatabac@yahoo.com>
2021-01-07 14:39:52 +0200

    created upload image function

--- data_handler.py
+++ data_handler.py
@@ -1,7 +1,7 @@
 import os
 import csv
 import time
-import datetime
+from datetime import datetime
 
 
 
@@ -20,12 +20,15 @@ def read_file(filename):
 
         for row in reader:
             mylist.append(row)
-
-        # for line in mylist:
-        #     if line['submission_time']:
-        #         line['submission_time'] = time.strftime("%d/%m/%Y %H:%M", time.localtime(int(line['submission_time'])))
-
         return mylist
+
+
+def create_question_id():
+    return create_id(QUESTIONS)
+
+
+def create_answer_id():
+    return create_id(ANSWERS)
 
 
 def create_id(filename):
@@ -38,19 +41,96 @@ def create_id(filename):
     return id_count
 
 
-def write_question(result):
-    timestamp = int(time.time())
-    result['id'] = create_id(QUESTIONS)
-    result['submission_time'] = timestamp
-    result['view_number'] = 0
-    result['vote_number'] = 0
-    result['image'] = ""
-    result['title'] = result['title'].capitalize()
-    result['message'] = result['message'].capitalize()
+def question_csv2obj(csv_dict):
+    # '7', '1609922180',  '2', '2', 'Loooollll' ,'Jgjhgjhb',
+    #  7,  datetime(...),  2,   2,  'Loooollll' ,'Jgjhgjhb',
+    # {
+    #     'id': '232',
+    #     'submission_time': '12234235435',
+    #     'view_number': '213',
+    #     'vote_number': '2',
+    #     'title': 'vceva',
+    #     'message': 'ceva pe linii',
+    #     'image': '',
+    # }
+    # {
+    #     'id': 232,
+    #     'submission_time': datetime.datetime(),
+    #     'view_number': 213,
+    #     'vote_number': 2,
+    #     'title': 'vceva',
+    #     'message': 'ceva pe linii',
+    #     'image': '',
+    # }
 
-    with open(QUESTIONS, "a", newline='') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=QUESTIONS_HEADER)
-        writer.writerow(result)
+
+    question_obj = {}
+    question_obj['id'] = int(csv_dict['id'])
+    question_obj['submission_time'] = datetime.fromtimestamp(int(float(csv_dict['submission_time'])))
+    # orice str care contine int devine pe bune un int
+    return question_obj
+
+
+def question_obj2csv(obj_dict):
+    csv_obj = {}
+    # csv_obj['id'] = str(obj_dict['id'])
+    csv_obj['submission_time'] = int(obj_dict['submission_time'].timestamp())
+    return csv_obj
+
+
+def _read_questions(question_id=None):
+    all_questions = read_file(QUESTIONS)
+    questions = []
+    if question_id is not None:
+        for question in all_questions:
+            if int(question['id']) == question_id:
+                questions.append(question)
+    else:
+        questions = all_questions
+
+    for question in questions:
+        question['submission_time'] = datetime.utcfromtimestamp(int(float(question['submission_time'])))
+        # item['vote_number'] = int(item['vote'])
+    return questions
+    # return [ question_csv2obj(q) for q in all_questions ]
+
+
+def read_answers(question_id):
+    pass
+
+
+def read_questions():
+    return _read_questions()
+
+
+def read_question(question_id):
+    questions = _read_questions(question_id)
+    if questions:
+        return questions[0]
+    else:
+        return None
+
+
+def write_question(result):
+    result['submission_time'] = int(datetime.now().timestamp())
+    result['submission_time'] = int(result['submission_time'])
+    
+    if not result.get('id'):
+        result['id'] = create_question_id()
+        with open(QUESTIONS, "a", newline='') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=QUESTIONS_HEADER)
+            writer.writerow(result)
+    else:
+        questions = read_file(QUESTIONS)
+        for item in questions:
+            if int(item['id']) == int(result['id']):
+                item.update(result)
+        with open(QUESTIONS, "w", newline='') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=QUESTIONS_HEADER)
+            writer.writeheader()
+
+            for item in questions:
+                writer.writerow(item)
 
 
 def write_answer(result, question_id):
@@ -60,7 +140,7 @@ def write_answer(result, question_id):
     result['vote_number'] = 0
     result['question_id'] = question_id
     result['message'] = result['message'].capitalize()
-    result['image'] = ''
+    
 
     with open(ANSWERS, "a", newline='') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=ANSWERS_HEADER)
@@ -77,10 +157,6 @@ def write_count(count, question_id):
         for line in dict_list:
             if int(line['id']) == question_id:
                 line['view_number'] = str(count)
-            # if line['submission_time']:
-            #     line['submission_time'] = int(time.mktime(datetime.datetime.strptime(line['submission_time'],
-            #                                                                          "%d/%m/%Y %H:%M").timetuple()))
-
             writer.writerow(line)
 
 
@@ -91,14 +167,15 @@ def delete_question(question_id):
         for row in reader:
             if int(row['id'])!= question_id:
                 myquestion.append(row)
-
+    
     with open(QUESTIONS, "w", newline='') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=QUESTIONS_HEADER)
         writer.writeheader()
-
+    
         for item in myquestion:
             writer.writerow(item)
-
+    
+   
     delete_question_answers(question_id)
 
 
@@ -106,6 +183,7 @@ def delete_question_answers(question_id):
     myanswers = []
     with open(ANSWERS, newline='') as csv_file:
         reader = csv.DictReader(csv_file)
+        
         for row in reader:
             if int(row['question_id']) != question_id:
                 myanswers.append(row)
@@ -136,17 +214,7 @@ def delete_answer(answer_id):
 
 
 def edit_question(edited_question, question_id):
-    questions = read_file(QUESTIONS)
-    for item in questions:
-        if int(item['id']) == int(edited_question['id']):
-            item.update(edited_question)
-    with open(QUESTIONS, "w", newline='') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=QUESTIONS_HEADER)
-        writer.writeheader()
-
-        for item in questions:
-            writer.writerow(item)
-
+    pass
 
 def vote_up_down(question_id, action):
     questions = read_file(QUESTIONS)
@@ -182,3 +250,20 @@ def vote_up_down_answer(answer_id, action):
 
         for item in answers:
             writer.writerow(item)
+
+
+
+# def get_time_submission():
+    
+#     list_of_questions = read_file(QUESTIONS)
+#     questions_with_transformed_submission_times = [int(submission['submission_time']) for submission in list_of_questions]
+#     return submission_time
+
+
+# def transform_time(list_time):
+#     list_of_dates = []
+
+#     for item in list_time:
+#         item = datetime.utcfromtimestamp(int(float(item)))
+#         list_of_dates.append(item)
+#     return list_of_dates
--- server.py
+++ server.py
@@ -1,99 +1,154 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, abort
 import data_handler
 import time
-import datetime
+from datetime import datetime
+from werkzeug.utils import secure_filename
+import os
+from uuid import uuid4 as uuid
+
 
 app = Flask(__name__)
+
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['UPLOAD_PATH'] = 'static/images'
 
 
 @app.route("/")
 @app.route("/list")
 def main_page():
-    questions = data_handler.read_file(data_handler.QUESTIONS)
-    values = dict(request.args)
-    order_by = values.get("order_by")
-    # for question in questions:
-    #         question['submission_time'] = time.strftime("%d/%m/%Y %H:%M", time.localtime(int(question['submission_time'])))
-    if "order_by" in values.keys():
-        order_by = values["order_by"]
-        if values["order_by"] in data_handler.QUESTIONS_HEADER:
-            if order_by in values.values() and "asc" in values.values():
-                questions = sorted(questions, key=lambda row: row.get(order_by))
-            else:
-                questions = sorted(questions, key=lambda row: row.get(order_by), reverse=True)
-            return render_template("list.html", questions=questions)
-        else:
-            return render_template("list.html", questions=questions)
-    else:
-        questions = sorted(questions, key=lambda row: row.get("submission_time"), reverse=True)
+    params_of_get = dict(request.args)
+    order_by = params_of_get.get("order_by", 'submission_time')
+    order_direction = params_of_get.get('order_direction', 'desc') 
+    rev = True if order_direction == 'desc' else False
 
-        return render_template("list.html", questions=questions)
+    questions = data_handler.read_questions()
+    questions = sorted(questions, key=lambda q: q[order_by], reverse=rev)
+
+    return render_template("list.html", questions=questions)
 
 
 @app.route("/question/<int:question_id>")
 def display_question(question_id):
     questions = data_handler.read_file(data_handler.QUESTIONS)
     answers = data_handler.read_file(data_handler.ANSWERS)
-    
-    show_questions = []
+
+    show_question = data_handler.read_question(question_id)
     show_answers = []
     
-
     for line in questions:
         count = int(line['view_number'])
         if int(line['id']) == question_id:
-            show_questions.append(line['title'])
-            show_questions.append(line['message'])
             count += 1
             data_handler.write_count(count, question_id)
 
     for line in answers:
         if int(line['question_id']) == question_id:
             show_answers.append(line)
-            # show_answers.append(line['message'])
-            # show_answers.append(int(line['id']))
-    
-            
 
-    return render_template('question.html', questions=questions, question_id=question_id, show_questions=show_questions, show_answers=show_answers)
+    return render_template('question.html', show_question=show_question, show_answers=show_answers, question_id=question_id)
 
 
 @app.route('/add-question', methods=['GET', 'POST'])
 def add_question():
     if request.method == "POST":
         result = dict(request.form)
+        uploaded_file = request.files.get("file")
+        print(uploaded_file)
+        filename = ''
+        if uploaded_file:
+            unique_name = uuid()
+            filename = str(unique_name) + secure_filename(uploaded_file.filename)
+            file_ext = os.path.splitext(filename)[1]
+            if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+                abort(400)
+            uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        
+        result['view_number'] = 0
+        result['vote_number'] = 0
+        result['title'] = result['title'].capitalize()
+        result['message'] = result['message'].capitalize()
+        result['image'] = filename
+
+        print(filename)
         data_handler.write_question(result)
         question_id = data_handler.create_id(data_handler.QUESTIONS)-1
+    
         return redirect(url_for("display_question", question_id=question_id))
     return render_template('add-question.html')
+
+
+@app.route('/question/<int:question_id>/edit', methods=['GET', 'POST'])
+def edit_question(question_id):
+        if request.method == "POST":
+            edited_question = dict(request.form)
+            edited_question["id"] = str(question_id)
+            print(edited_question)
+            data_handler.write_question(edited_question)
+            return redirect(url_for("display_question", question_id=question_id))
+            
+        elif request.method == 'GET':  # OPTIONS, HEAD, PUT, PATCH, DELETE (HTTP methods...)
+            item = data_handler.read_question(question_id)
+            if not item:
+                abort(404)
+            return render_template("edit-question.html", question=item)
 
 
 @app.route('/question/<int:question_id>/new-answer', methods=['GET', 'POST'])
 def add_answer(question_id):
     if request.method == "POST":
         result = dict(request.form)
+        uploaded_file = request.files.get("file")
+        print(uploaded_file)
+        filename = ''
+        if uploaded_file:
+            unique_name = uuid()
+            filename = str(unique_name) + secure_filename(uploaded_file.filename)
+            file_ext = os.path.splitext(filename)[1]
+            if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+                abort(400)
+            uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        result['image'] = filename
         data_handler.write_answer(result, question_id)
         return redirect(url_for("display_question", question_id=question_id))
+
     return render_template('add-answer.html', question_id=question_id)
 
 
 @app.route('/question/<int:question_id>/delete', methods=['POST'])
 def delete_question(question_id):
+    all_answers = data_handler.read_file(data_handler.ANSWERS)
+    all_question = data_handler.read_file(data_handler.QUESTIONS)
     if request.method == "POST":
+        for row in all_question:
+            if int(row['id']) == question_id:
+                try:
+                    os.remove("static/images/" + row['image'])
+                except:
+                    pass
+
+
+        for row in all_answers:
+            if int(row['question_id']) == question_id:
+                os.remove("static/images/" + row['image'])
+        
         data_handler.delete_question(question_id)
         return redirect("/list")
+
     return redirect("/list")
 
 
 @app.route('/answer/<int:answer_id>/delete', methods=['GET', 'POST'])
 def delete_answer(answer_id):
-    
+    all_answers = data_handler.read_file(data_handler.ANSWERS)
     if request.method == "POST":
-        all_answers = data_handler.read_file(data_handler.ANSWERS)
-
         for row in all_answers:
             if int(row['id']) == answer_id:
                 question_id = int(row['question_id'])
+                try:
+                    os.remove("static/images/" + row['image'])
+                except:
+                    pass
         
         data_handler.delete_answer(answer_id)
         question_url = url_for('display_question', question_id=question_id)
@@ -101,35 +156,17 @@ def delete_answer(answer_id):
         return redirect(question_url)
     
 
-@app.route('/question/<int:question_id>/edit', methods=['GET', 'POST'])
-def edit_question(question_id):
-        if request.method == "POST":
-            all_questions = data_handler.read_file(data_handler.QUESTIONS)
-            edited_question = dict(request.form)
-            edited_question["id"] = edited_question.get(id, str(question_id))
-            print(edited_question)
-            data_handler.edit_question(edited_question, question_id)
-            return redirect(url_for("display_question", question_id=question_id))
-            
-        else:
-            question_for_edit = []
-            all_questions = data_handler.read_file(data_handler.QUESTIONS)
-            for question in all_questions:
-                if int(question['id']) == question_id:
-                    question_for_edit.append(question)
-            
-            return render_template("edit-question.html", question_for_edit=question_for_edit)
-
-
 @app.route('/question/<int:question_id>/vote_up')
 def vote_up_question(question_id):
     data_handler.vote_up_down(question_id, "up")
+
     return redirect('/list')
 
 
 @app.route('/question/<int:question_id>/vote_down')
 def vote_down_question(question_id):
     data_handler.vote_up_down(question_id, "down")
+
     return redirect('/list')
 
 
@@ -139,7 +176,9 @@ def vote_up_answer(answer_id):
     for row in all_answers:
         if int(row['id']) == answer_id:
             question_id = int(row['question_id'])
+
     data_handler.vote_up_down_answer(answer_id, "up")
+
     return redirect(url_for('display_question', question_id=question_id))
 
 
@@ -149,7 +188,9 @@ def vote_down_answer(answer_id):
     for row in all_answers:
         if int(row['id']) == answer_id:
             question_id = int(row['question_id'])
+
     data_handler.vote_up_down_answer(answer_id, "down")
+
     return redirect(url_for('display_question', question_id=question_id))
 
 
